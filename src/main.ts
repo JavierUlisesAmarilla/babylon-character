33591cc9f17a6301834fb1614a23e53c57d72976
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-05-10 10:16:58 -0300

    remove unnecessary files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { loadBabylon } from './babylon'
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')
-
 if (app) {
   loadBabylon()
 }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,21 +1,9 @@
 import * as BABYLON from 'babylonjs'
 
-export const getRandomTarget = (origin: BABYLON.Vector3, zOffset: number, range: number) => {
-  const dX = Math.random() * range - range / 2
-  const dY = Math.random() * range - range / 2
-  const target = new BABYLON.Vector3(origin.x + dX, origin.y + dY, origin.z + zOffset)
-  return target
-}
-
 export const getYLookQuat = (origin: BABYLON.Vector3, target: BABYLON.Vector3) => {
   return BABYLON.Quaternion.FromRotationMatrix(BABYLON.Matrix.LookAtLH(origin, target, BABYLON.Axis.Y).invert())
 }
 
 export const getZLookQuat = (origin: BABYLON.Vector3, target: BABYLON.Vector3) => {
   return BABYLON.Quaternion.FromRotationMatrix(BABYLON.Matrix.LookAtLH(origin, target, BABYLON.Axis.Z).invert())
-}
-
-export const delay = (s: number) => {
-  if (s <= 0) return Promise.resolve()
-  return new Promise((resolve) => setTimeout(resolve, s * 1e3))
 }
